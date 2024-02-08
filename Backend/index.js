0e4205d5afd842ab4d5bc782db1a1862ea468431
ayanahmad11 <ayan2011032@akgec.ac.in>
2024-02-08 20:10:03 +0530

    first

---
+++ Backend/index.js
@@ -0,0 +1,29 @@
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const port = 3000;
+const authRoutes = require("./routes/auth");
+const todoRoutes = require("./routes/todo");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+app.use("/auth", authRoutes);
+app.use("/todo", todoRoutes);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+// Set up connection URI
+const uri = 'mongodb+srv://ayan005aa:8wOgqn8mm6cuKEeR@cluster0.rvgmrdn.mongodb.net' // Replace 'mydatabase' with your actual database name
+
+// Connect to MongoDB
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
