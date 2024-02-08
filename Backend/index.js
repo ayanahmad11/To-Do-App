b0a2eb6b581798d5760ad7a6e08c5a29442c5168
ayanahmad11 <ayan2011032@akgec.ac.in>
2024-02-08 21:07:29 +0530

    again

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,13 @@ const port = 3000;
 const authRoutes = require("./routes/auth");
 const todoRoutes = require("./routes/todo");
 const cors = require("cors");
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://to-do-app1-nu.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
 app.use(cors());
 app.use(cors({
     origin:["https://to-do-app1-nu.vercel.app"],
