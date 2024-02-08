e3589137f82986efbc2d6643e1394b56d357849f
ayanahmad11 <ayan2011032@akgec.ac.in>
2024-02-08 21:02:59 +0530

    again

--- Backend/index.js
+++ Backend/index.js
@@ -8,6 +8,11 @@ const todoRoutes = require("./routes/todo");
 const cors = require("cors");
 
 app.use(cors());
+app.use(cors({
+    origin:["https://to-do-app1-nu.vercel.app"],
+    methods:[POST,GET,DELETE]
+  
+}))
 app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/todo", todoRoutes);
