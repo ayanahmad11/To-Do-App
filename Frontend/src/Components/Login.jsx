a7d1ec8b6d917984e058ede9ca87e44907843693
ayanahmad11 <ayan2011032@akgec.ac.in>
2024-02-14 17:28:42 +0530

    14-2-24

--- Frontend/src/Components/Login.jsx
+++ Frontend/src/Components/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     
         if (data.token) {
             localStorage.setItem("token", data.token);
-            window.location.pathname = '/todo'; 
+            window.location.pathname = '/'; 
         } else {
             alert("Invalid credentials");
         }
