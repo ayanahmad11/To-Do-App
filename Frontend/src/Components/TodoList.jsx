0b9a436103b2458eba3ee97cdd124319dbd5d6d5
ayanahmad11 <ayan2011032@akgec.ac.in>
2024-02-14 20:56:10 +0530

    removed error of todo.css

--- Frontend/src/Components/TodoList.jsx
+++ Frontend/src/Components/TodoList.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { authState } from '../store/authState.js';
 import { useRecoilValue } from "recoil";
-import './Todolist.css';
+
 
 const TodoList = () => {
     const [todos, setTodos] = useState([]);
@@ -71,7 +71,7 @@ const TodoList = () => {
                     <p style={{ marginTop: '5px', marginBottom: '10px', color: '#666' }}>{todo.description}</p>
                     
                     <button onClick={() => markDone(todo._id)} 
-                    style={{ backgroundColor: todo.done ? '#757575' : '#1976d2', color: '#fff', border: 'none', padding: '8px 16px', borderRadius: '4px', cursor: 'pointer', marginRight: '10px',marginBottom: "10px" }}>
+                    style={{ backgroundColor: todo.done ? '#757575' : '#1976d2', color: '#fff', border: 'none', padding: '8px 16px', borderRadius: '4px', cursor: 'pointer', marginRight: '10px',marginBottom: '10px' }}>
                         {todo.done ? 'Done' : 'Mark as Done'}</button>
                       
                     <button onClick={() => deleteTodo(todo._id)} style={{ backgroundColor: '#d32f2f', color: '#fff', border: 'none', padding: '8px 16px', borderRadius: '4px', cursor: 'pointer' }}>Delete</button>
